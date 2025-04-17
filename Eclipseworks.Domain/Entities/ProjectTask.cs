699bc0a3ab3e1929e4d6696442f898a51e15e6ad
danielgmarconi <danielgmarconi@gmail.com>
2025-04-17 09:59:04 -0300

    Implementações

--- Eclipseworks.Domain/Entities/ProjectTask.cs
+++ Eclipseworks.Domain/Entities/ProjectTask.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Eclipseworks.Domain.Enums;
+﻿using Eclipseworks.Domain.Enums;
 
 namespace Eclipseworks.Domain.Entities
 {
@@ -14,11 +9,25 @@ namespace Eclipseworks.Domain.Entities
         public int ProjectId { get; set; }
         public Project Project { get; set; }
         public ICollection<ProjectTaskComment>? ProjectTaskComments { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public DateTime? StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
-        public PriorityStatus Priority { get; set; }
-        public ProjectTaskStatus Status { get; set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public DateTime? StartDate { get; private set; }
+        public DateTime? EndDate { get; private set; }
+        public PriorityStatus Priority { get;private set; }
+        public ProjectTaskStatus Status { get; private set; }
+        public Int16 TimeHoursTask { get; private set; }
+        public ProjectTask(int userid,
+                           int projectId,
+                           string name,
+                           string description,
+                           DateTime? startDate,
+                           DateTime? endDate,
+                           PriorityStatus priority,
+                           ProjectTaskStatus status,
+                           Int16 timeHoursTask
+                           )
+        {
+            
+        }
     }
 }
